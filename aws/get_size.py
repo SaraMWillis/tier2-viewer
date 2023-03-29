074218660ab83f2f0e6311c86cf30b5613463894
Sara Willis <sarawillis@email.arizona.edu>
2023-03-29 12:31:04 -0700

    Added more useful comments

--- aws/get_size.py
+++ aws/get_size.py
@@ -3,9 +3,20 @@
 import subprocess, json, sys
 from tqdm import tqdm
 
+'''
+------------------------------------------------------------------------------------------
+                                     du
+Get the size of a "directory" or object. AWS doesn't have actual directories, so this 
+really is getting the total size of objects starting with a particular prefix. This is 
+roughly equivalent to running du -sh on a unix operating system.
+'''
 def du(user_args):
+    
+    # AWS pulls a number of objects at a time. The number is defined by page_size here.
     page_size = 10000
     if user_args.prefix == None:
+        
+        # If we're not targeting a specific directory, we query the whole bucket.
         p = subprocess.Popen(['aws s3api list-objects --bucket %s --query "[length(Contents[])]"'%(user_args.bucket)],stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
     else:
         p = subprocess.Popen(['aws s3api list-objects --bucket %s --prefix=%s --query "[length(Contents[])]"'%(user_args.bucket,user_args.searchable_prefix)],stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
@@ -14,16 +25,21 @@ def du(user_args):
     total_objects = int(total_objects[0])
     print("Total objects found: %s"%total_objects)
     
-    
+    # If more objects were found than the max we can grab at a time, we'll create a progress bar that users can 
+    # use to get an idea of how their request is processing
     if page_size < total_objects:
-        #print("Processing...")
         pbar = tqdm(total = total_objects,ascii=True, dynamic_ncols=True,leave=False)
+        
+    # AWS will generate a starting token that will allow you to move through the contents of your bucket
+    # in chunks. 
     next_token = None
     size = 0 
     while True:
         if next_token == None:
+            # We start without supplying a token flag in our AWS query
             next_token_flag = ""
         else:
+            # For any subsequent searches, we add --starting-token <token> to our query
             next_token_flag = "--starting-token %s"%next_token
         if user_args.prefix == None:
             p = subprocess.Popen(['aws s3api list-objects-v2 --bucket %s --max-items %s %s'%(user_args.bucket,page_size,next_token_flag)],stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
@@ -38,11 +54,8 @@ def du(user_args):
             next_token = out["NextToken"]
             if page_size < total_objects:
                 pbar.update(page_size)
-
         else:
             break
     if page_size < total_objects:
         pbar.update(page_size%total_objects)
-
-
     return size
