9f762e88c9a0ce1e90db565cd1115f98ee9e3a94
Sara Willis <sarawillis@email.arizona.edu>
2023-03-29 13:18:38 -0700

    added comments

--- aws/merge.py
+++ aws/merge.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
+'''
+------------------------------------------------------------------------------------------
+                                     Merge Bucket Dicts
+A small function that takes the dictionaries generated from the output of aws s3 ls
+and aws s3api list-objects and merges them.
+
+Not particularly impressive, but it gives us some additional information about 
+objects that were found with ls that can be used to skip running a head-object on
+every key later.
+'''
+
 def merge_bucket_dictionaries(ls_bucket,list_objects_bucket):
     for key,value in list_objects_bucket.items():
         if key in ls_bucket.keys():
